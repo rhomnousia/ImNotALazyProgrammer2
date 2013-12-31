9d528ab7333f3be95b837a65673c7e57f5d838b8
rhomnousia <johanthegnarler@gmail.com>
2013-12-31 17:45:51 -0500

    all files

---
+++ src/WorkTimerGUI/MainForm.java
@@ -0,0 +1,12 @@
+package WorkTimerGUI;
+
+import javax.swing.*;
+
+/**
+ * Created by Johan on 12/31/13.
+ */
+public class MainForm {
+    private JPanel mainForm;
+    private JButton button1;
+    private JButton button2;
+}
